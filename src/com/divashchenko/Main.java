6d1852dbd951b11bca0155af399648e2f7429506
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-30 20:21:14 +0300

    add tests

--- src/com/divashchenko/Main.java
+++ src/com/divashchenko/Main.java
@@ -5,7 +5,15 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
+
+        List<User> users = new ArrayList<>();
+        users.add(new User(0, "Alex"));
+        users.add(new User(1, "Mike"));
+        users.add(new User(2, "Olga"));
+        users.add(new User(0, "Bill"));
+
+        System.out.println(users);
+        System.out.println(deleteUsersDuplicates(users));
     }
 
     private static List<User> deleteUsersDuplicates(List<User> list) {
