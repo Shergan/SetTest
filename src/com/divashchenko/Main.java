e2c93ae7207c5edfc04b3dcc7ecc63c43fcc00ef
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-30 20:20:04 +0300

    add deleteUsersDuplicates method

--- src/com/divashchenko/Main.java
+++ src/com/divashchenko/Main.java
@@ -1,8 +1,14 @@
 package com.divashchenko;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
+    }
+
+    private static List<User> deleteUsersDuplicates(List<User> list) {
+        return new ArrayList<>(new LinkedHashSet<>(list));
     }
 }
