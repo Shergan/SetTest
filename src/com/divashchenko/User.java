edfd05c48b7292103b656aab8b56d90c30fc09d8
Shergan <dmitriy.ivashchenko.work@gmail.com>
2019-04-30 20:21:02 +0300

    add equals hashCode

--- src/com/divashchenko/User.java
+++ src/com/divashchenko/User.java
@@ -1,5 +1,7 @@
 package com.divashchenko;
 
+import java.util.Objects;
+
 public class User {
 
     private int id;
@@ -27,6 +29,19 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
